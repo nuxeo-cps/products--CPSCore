d8ff66862180c7dc8257d4a2f319414c9dc1ad2c
janguenot <devnull@localhost>
2003-08-07 10:21:20 +0000

    CMF actions filtered for CPS

--- CPSMembershipTool.py
+++ CPSMembershipTool.py
@@ -1,8 +1,9 @@
 # (C) Copyright 2002, 2003 Nuxeo SARL <http://nuxeo.com>
 # Authors: Florent Guillaime <fg@nuxeo.com>
 #          Alexandre Fernandez <alex@nuxeo.com>
+#          Julien Anguenot <ja@nuxeo.com>
 #
-# 
+#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 as published
 # by the Free Software Foundation.
@@ -20,12 +21,14 @@
 #
 # Replace MonkeyPatch of Membershiptool by real object use
 #
-# $Id$ 
+# $Id$
 
 from Globals import InitializeClass
 from AccessControl import ClassSecurityInfo
 
 from Products.CMFCore.CMFCorePermissions import View, ManagePortal
+from Products.CMFCore.ActionsTool import ActionInformation as AI
+from Products.CMFCore.Expression import Expression
 from Products.CMFDefault.MembershipTool import MembershipTool
 from Products.NuxUserGroups.CatalogToolWithGroups import mergedLocalRoles
 from Products.NuxUserGroups.LocalRolesWithGroups import \
@@ -33,16 +36,53 @@ from Products.NuxUserGroups.LocalRolesWithGroups import \
 
 from zLOG import LOG, DEBUG
 
-# XXX : to move somewhere else 
+# XXX : to move somewhere else
 WORKSPACES = "workspaces"
-MEMBERS    = "members" 
+MEMBERS    = "members"
 
 class CPSMembershipTool(MembershipTool):
+    """ Replace MonkeyPatch of Membershiptool by real object use."""
+
+    #
+    # Actions for CPS
+    #
+
+    _actions =[
+      AI( id='login'
+        , title='Login'
+        , description='Click here to Login'
+        , action=Expression(text='string:${portal_url}/login_form')
+        , permissions=(View,)
+        , category='user'
+        , condition=Expression(text='not: member')
+        , visible=1
+        )
+    , AI( id='logout'
+        , title='Log out'
+        , description='Click here to logout'
+        , action=Expression(text='string:${portal_url}/logout')
+        , permissions=(View,)
+        , category='user'
+        , condition=Expression(text='member')
+        , visible=1
+        )
+    , AI( id='mystuff'
+        , title='My stuff'
+        , description='Goto your home folder'
+        , action=Expression(text='string:${portal/portal_membership'
+                               + '/getHomeUrl}/folder_contents')
+        , permissions=(View,)
+        , category='user'
+        , condition=Expression( text='python: member and '
+                              + 'portal.portal_membership.getHomeFolder()')
+        , visible=1
+        )
+    ]
 
     meta_type = 'CPS Membership Tool'
 
     security = ClassSecurityInfo()
-    
+
     security.declareProtected(View, 'getMergedLocalRoles')
     def getMergedLocalRoles(self, object, withgroups=1, withpath=0):
         """
@@ -86,7 +126,7 @@ class CPSMembershipTool(MembershipTool):
     security.declareProtected(ManagePortal, 'createMemberarea')
     def createMemberarea(self, member_id):
         """Create a member area."""
-        
+
         parent = self.aq_inner.aq_parent
         ws_root =  getattr(parent, WORKSPACES, None)
         members =  getattr(ws_root, MEMBERS, None)
@@ -96,16 +136,16 @@ class CPSMembershipTool(MembershipTool):
         if members is not None and user is not None:
             f_title = "%s's Home" % member_id
 
-            # XXX : GOTTA FIX THAT IN A MORE PROPER WAY 
+            # XXX : GOTTA FIX THAT IN A MORE PROPER WAY
             # Hack to pass workflow guards
             manage_setLocalGroupRoles(members, 'role:Anonymous',
                                       ['WorkspaceManager'] )
 
             members.invokeFactory('Workspace', member_id)
-             
-            manage_delLocalGroupRoles(members, ['role:Anonymous'])            
+
+            manage_delLocalGroupRoles(members, ['role:Anonymous'])
             # TODO set workspace properties ? title ..
-            f=getattr(members, member_id)            
+            f=getattr(members, member_id)
             # Grant ownership to Member
             try: f.changeOwnership(user)
             except AttributeError:
@@ -115,25 +155,30 @@ class CPSMembershipTool(MembershipTool):
     security.declarePublic('getHomeFolder')
     def getHomeFolder(self, id=None, verifyPermission=0):
         """ Return a member's home folder object, or None."""
-        
+
         if id is None:
-            member = self.getAuthenticatedMember()    
+            member = self.getAuthenticatedMember()
             if not hasattr(member, 'getMemberId'):
                 return None
-            id = member.getMemberId()        
+            id = member.getMemberId()
         parent = self.aq_inner.aq_parent
-        
+
         ws_root =  getattr(parent, WORKSPACES, None)
         try:
             members =  getattr(ws_root, MEMBERS, None)
-            folder  =  getattr(members, id, None)            
+            folder  =  getattr(members, id, None)
             if verifyPermission and not _checkPermission('View', folder):
                 # Don't return the folder if the user can't get to it.
-                return None            
-            return folder        
+                return None
+            return folder
         except KeyError:
             pass
         return None
+
+    security.declarePrivate( 'listActions' )
+    def listActions(self, info=None):
+        """List actions available through the tool."""
+        return self._actions
 
 InitializeClass(MembershipTool)
 
