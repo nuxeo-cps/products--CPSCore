648bfd0ce0cb3aac9e312398333d8baeb03afbd7
sfermigier <devnull@localhost>
2004-03-15 20:45:48 +0000

    Add copyright test.

---
+++ tests/test_misc.py
@@ -0,0 +1,14 @@
+import unittest
+from Testing.ZopeTestCase import ZopeTestCase
+
+class TestMisc(ZopeTestCase):
+    def testCopyright(self):
+        copyright = self._app().manage_copyright()
+        self.assert_(copyright.find("Nuxeo CPS") >=0)
+
+def test_suite():
+    loader = unittest.TestLoader()
+    return loader.loadTestsFromTestCase(TestMisc)
+
+if __name__=='__main__':
+    unittest.TextTestRunner().run(test_suite())
