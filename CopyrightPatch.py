f195e0467618b25e3c2b2727eb0ef3eb51a46020
edegoute <devnull@localhost>
2004-10-28 15:27:55 +0000

    update release number

--- CopyrightPatch.py
+++ CopyrightPatch.py
@@ -2,7 +2,7 @@ from Globals import DTMLFile
 from ZServer.HTTPServer import zhttp_server
 from App.Management import Navigation
 
-zhttp_server.SERVER_IDENT += ' CPS/3.0'
+zhttp_server.SERVER_IDENT += ' CPS/3.2'
 
 manage_copyright = DTMLFile('zmi/copyright', globals())
     
