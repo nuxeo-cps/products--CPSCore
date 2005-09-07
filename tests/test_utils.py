b52851c2f518d229a2d38deee74214abb49aafd5
janguenot <devnull@localhost>
2005-09-07 17:23:41 +0000

    Add test for #904

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -14,6 +14,22 @@ class Test(unittest.TestCase):
         self.assert_(utils._isinstance(a, A))
         self.failIf(utils._isinstance(b, A))
 
+    def test_mergedLocalRolesManipulation(self):
+        # The _mergedLocalRoles function used to return references to
+        # actual local role settings and it was possible to manipulate them
+        # by changing the return value. http://www.zope.org/Collectors/CMF/376
+        from Products.CMFCore.tests.base.dummy import DummyContent
+        from Products.CPSCore.utils import mergedLocalRoles
+        obj = DummyContent()
+        obj.manage_addLocalRoles('dummyuser1', ['Manager', 'Owner'])
+        self.assertEqual(len(obj.get_local_roles_for_userid('dummyuser1')), 2)
+
+        merged_roles = mergedLocalRoles(obj)
+        merged_roles['dummyuser1'].append('FOO')
+
+        # The values on the object itself should still the the same
+        self.assertEqual(len(obj.get_local_roles_for_userid('dummyuser1')), 2)
+
 def test_suite():
     loader = unittest.TestLoader()
     return loader.loadTestsFromTestCase(Test)
