f6903b209e6db59fe1c3161540e33dc7e028e5df
fguillaume <devnull@localhost>
2005-09-19 20:16:04 +0000

    #872: Proper upgrades for catalog broken by CPS 3.3.5 (unicode errors) Unit tests for the upgrade.

---
+++ tests/test_upgrade_catalog.py
@@ -0,0 +1,124 @@
+# (C) Copyright 2005 Nuxeo SARL <http://nuxeo.com>
+# Author: Florent Guillaume <fg@nuxeo.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+# 02111-1307, USA.
+#
+# $Id$
+"""Tests for catalog upgrade (3.3.5 to 3.3.6).
+"""
+
+import unittest
+from Testing import ZopeTestCase
+
+from OFS.Folder import Folder
+from OFS.SimpleItem import SimpleItem
+from Products.CMFCore.CatalogTool import CatalogTool
+from Products.ZCTextIndex.Lexicon import Splitter
+from Products.ZCTextIndex.Lexicon import CaseNormalizer
+from Products.ZCTextIndex.Lexicon import StopWordRemover
+from Products.CMFCore.utils import SimpleRecord
+
+ZopeTestCase.installProduct('ZCTextIndex', quiet=1)
+
+
+class FakeObject(SimpleItem):
+    title = 'fake'
+    def __init__(self, id):
+        self._setId(id)
+    def Title(self):
+        return self.title
+    def ZCTitle(self):
+        return self.title
+    def reindexObject(self):
+        self.portal_catalog.reindexObject(self)
+
+class SimpleCatalogTool(CatalogTool):
+    def enumerateIndexes(self):
+        return (
+            ('Title', 'FieldIndex', None),
+            ('ZCTitle', 'ZCTextIndex',
+                 SimpleRecord(lexicon_id='cps_defaut_lexicon',
+                              index_type='Okapi BM25 Rank')),
+            )
+    def enumerateColumns(self):
+        return (
+            'Title',
+            )
+    def enumerateLexicons(self):
+        return (('cps_defaut_lexicon', # sic
+                 Splitter(),
+                 CaseNormalizer(),
+                 StopWordRemover(),
+                 ),
+                )
+
+class Upgrade335to336TestCase(unittest.TestCase):
+
+    def _makeOne(self):
+        self.root = Folder('')
+        self.root.portal = Folder('portal')
+        portal = self.root.portal
+        portal.portal_catalog = SimpleCatalogTool()
+
+
+    def test_upgrade(self):
+        from Products.CPSCore.upgrade import upgrade_335_336_catalog
+        self._makeOne()
+        portal = self.root.portal
+        catalog = portal.portal_catalog
+
+        # Catalog old object with unicode title
+        portal.workspaces = FakeObject('workspaces')
+        workspaces = portal.workspaces
+        workspaces.title = u"caf\xe9"
+        catalog.indexObject(workspaces)
+
+        # Even if we fix its title to str, we can't reindex it
+        # because changing metadata has problems with current Zope.
+        workspaces.title = "caf\xe9"
+        # Note:
+        # If the following test fails, it's because python has a
+        # sys.getdefaultencoding() to something not 'ascii'.
+        # Don't do that! Always use 'ascii'
+        self.assertRaises(UnicodeDecodeError, workspaces.reindexObject)
+        # (reset to buggy state to test upgrade later)
+        workspaces.title = u"caf\xe9"
+
+        # We can't insert any other object with latin1 title either
+        obb = FakeObject('obb')
+        obb.title = "ol\xe9"
+        portal.obb = obb
+        self.assertRaises(UnicodeDecodeError, catalog.indexObject, portal.obb)
+
+        # Fixup the catalog
+        res = upgrade_335_336_catalog(portal)
+        self.assertEquals(res, "Cleaned up: 1 index entries, 1 metadata entries, "
+                          "1 lexicon entries, 1 objects")
+
+        # Check workspace title is fixed
+        self.assertEquals(type(workspaces.title), str)
+        # Check we can reindex fixed workspaces
+        workspaces.reindexObject()
+        # Check we can index non-unicode accents now
+        catalog.indexObject(portal.obb)
+
+
+def test_suite():
+    return unittest.TestSuite((
+        unittest.makeSuite(Upgrade335to336TestCase),
+        ))
+
+if __name__ == '__main__':
+    unittest.TextTestRunner().run(test_suite())
--- upgrade.py
+++ upgrade.py
@@ -17,6 +17,10 @@
 #
 # $Id$
 
+from Products.PluginIndexes.common.UnIndex import UnIndex
+
+
+
 def upgrade_334_335_repository(context):
     """Upgrade the repository to remove security synthesis remnants.
 
@@ -42,3 +46,104 @@ def upgrade_334_335_repository(context):
             if (count % PARTIAL_COMMIT_EVERY) == 0:
                 get_transaction().commit(1)
     return "%s repository objects updated" % count
+
+
+
+def upgrade_335_336_catalog(context):
+    """Upgrade the catalog to cleanup catalogof unicode.
+
+    Fixes indexes, metadata and lexicons.
+    Also fixes bad objects that still have unicode titles.
+    """
+    INDEX_NAMES = ('Title', 'SearchableText')
+    METADATA_NAMES = ('Title',)
+    LEXICON_NAMES = ('cps_defaut_lexicon',) # sic
+    OB_ATTRIBUTES = ('title',)
+    RPATHS = ('sections', 'workspaces', 'workspaces/members')
+
+    catalog = context.portal_catalog
+    cat = catalog._catalog
+    nbindexes = 0
+    nbmetadata = 0
+    nblexicons = 0
+    nbobjects = 0
+
+    # Indexes
+    for index_name in INDEX_NAMES:
+        index = cat.indexes.get(index_name)
+        if index is None:
+            continue
+        if not isinstance(index, UnIndex):
+            continue
+        _index = index._index
+        _unindex = index._unindex
+        # Check if there's some unicode somewhere
+        bad = []
+        for key, value in _index.iteritems():
+            if isinstance(key, unicode):
+                bad.append((key, value))
+                nbindexes += 1
+        for key, value in bad:
+            del _index[key]
+        for key, value in bad:
+            key = key.encode('ISO-8859-15')
+            _index[key] = value
+            _unindex[value] = key
+
+    # Metadata
+    ixs = [cat.schema.get(metadata_name)
+           for metadata_name in METADATA_NAMES]
+    ixs = [i for i in ixs if i is not None]
+    # Check if there's some unicode in the metadata
+    bad = []
+    for key, value in cat.data.iteritems():
+        for i in ixs:
+            if isinstance(value[i], unicode):
+                bad.append(key)
+                nbmetadata += 1
+                break
+    for key in bad:
+        value = list(cat.data[key])
+        for i in ixs:
+            if isinstance(value[i], unicode):
+                value[i] = value[i].encode('ISO-8859-15')
+        cat.data[key] = tuple(value)
+
+    # Lexicons
+    for lexicon_name in LEXICON_NAMES:
+        lexicon = getattr(catalog, lexicon_name, None)
+        if lexicon is None:
+            continue
+        # Check if some words are unicode
+        bad = []
+        for word, wid in lexicon._wids.iteritems():
+            if isinstance(word, unicode):
+                bad.append((word, wid))
+                nblexicons += 1
+        for word, wid in bad:
+            del lexicon._wids[word]
+        for word, wid in bad:
+            word = word.encode('ISO-8859-15')
+            lexicon._wids[word] = wid
+            lexicon._words[wid] = word
+
+    # Fixup object who have old unicode attributes
+    for rpath in RPATHS:
+        proxy = context.restrictedTraverse(rpath, default=None)
+        if proxy is None:
+            continue
+        langs = proxy.getLanguageRevisions().keys()
+        for lang in langs:
+            doc = proxy.getContent(lang=lang)
+            some = False
+            for attr in OB_ATTRIBUTES:
+                v = getattr(doc, attr, None)
+                if isinstance(v, unicode):
+                    setattr(doc, attr, v.encode('ISO-8859-15'))
+                    some = True
+            if some:
+                nbobjects += 1
+
+    return ("Cleaned up: %s index entries, %s metadata entries, "
+            "%s lexicon entries, %s objects" % (
+            nbindexes, nbmetadata, nblexicons, nbobjects))
