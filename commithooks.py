2a494a0f5f11b409864fc98c0417e622e7698bd1
janguenot <devnull@localhost>
2005-12-22 14:50:28 +0000

    AfterCommitSubscribersManager on top of the ZODB transaction

--- commithooks.py
+++ commithooks.py
@@ -45,14 +45,23 @@ import bisect
 import transaction
 import zope.interface
 
+from Products.CPSCore.interfaces import ICommitSubscriber
 from Products.CPSCore.interfaces import IBeforeCommitSubscriber
+from Products.CPSCore.interfaces import IAfterCommitSubscriber
 from Products.CPSCore.interfaces import IZODBBeforeCommitHook
+from Products.CPSCore.interfaces import IZODBAfterCommitHook
 
-class BeforeCommitSubscriber(object):
-    """Base Manager definition
+_CPS_BCH_TXN_ATTRIBUTE = '_cps_before_commit_hooks_manager'
+_CPS_ACH_TXN_ATTRIBUTE = '_cps_after_commit_hooks_manager'
+
+#
+# Commit subsriber definitions
+#
+
+class CommitSubscriber(object):
+    """Base commit subscriber definition
     """
-
-    zope.interface.implements(IBeforeCommitSubscriber)
+    zope.interface.implements(ICommitSubscriber)
 
     # Not synchronous by default
     # XXX This may be monkey-patched by unit-tests.
@@ -67,20 +76,17 @@ class BeforeCommitSubscriber(object):
         mgr.addSubscriber(self, order=order)
 
     def setSynchronous(self, sync):
-        """Set queuing mode."""
         if sync:
             self()
         self._sync = sync
 
     def isSynchronous(self):
-        """Get queuing mode."""
         return self._sync
 
     def __call__(self):
         raise NotImplementedError
 
     def push(self, *args):
-
         raise NotImplementedError
 
     def enable(self):
@@ -89,7 +95,22 @@ class BeforeCommitSubscriber(object):
     def disable(self):
         self._status = False
 
-_CPS_BCH_TXN_ATTRIBUTE = '_cps_before_commit_hooks_manager'
+class BeforeCommitSubscriber(CommitSubscriber):
+    """Before commit subscriber definition
+    """
+    zope.interface.implements(IBeforeCommitSubscriber)
+
+class AfterCommitSubscriber(BeforeCommitSubscriber):
+    """After commit subscriber definition
+    """
+    zope.interface.implements(IAfterCommitSubscriber)
+
+    def __call__(self, status=True):
+        raise NotImplementedError
+
+#
+# Commit subscriber manager definitions
+#
 
 class BeforeCommitSubscribersManager(BeforeCommitSubscriber):
     """Holds subscribers that will be executed at the end of the transaction.
@@ -202,6 +223,128 @@ class BeforeCommitSubscribersManager(BeforeCommitSubscriber):
 
         self.log.debug("__call__ done")
 
+class AfterCommitSubscribersManager(AfterCommitSubscriber):
+    """Holds subscribers that will be executed after the
+    """
+
+    zope.interface.implements(IAfterCommitSubscriber, IZODBAfterCommitHook)
+
+    def __init__(self, txn):
+        """Initialize and register this manager with the transaction.
+        """
+        self._sync = self.DEFAULT_SYNC
+        self._status = self.DEFAULT_STATUS
+
+        # List of (order, index, hook, args, kws) tuples added by
+        # addAfterCommitHook().  `index` is used to resolve ties on equal
+        # `order` values, preserving the order in which the hooks were
+        # registered.  Each time we append a tuple to _before_commit,
+        # the current value of _before_commit_index is used for the
+        # index, and then the latter is incremented by 1.
+        self._after_commit = []
+        self._after_commit_index = 0
+        txn.addAfterCommitHook(self)
+
+        self.log = logging.getLogger(
+            "CPSCore.commithooks.AfterCommitSubscribersManager")
+
+    def addSubscriber(self, subscriber, args=(), kws=None, order=0):
+        """Register a subscriber to call after a transaction commit attempt.
+
+         The specified subscriber function will be called after the
+         transaction commit succeeds or aborts.  The first argument
+         passed to the subscriber is a Boolean value, true if the
+         commit succeeded, or false if the commit aborted.  `args`
+         specifies additional positional, and `kws` keyword, arguments
+         to pass to the subscriber.  `args` is a sequence of
+         positional arguments to be passed, defaulting to an empty
+         tuple (only the true/false success argument is passed).
+         `kws` is a dictionary of keyword argument names and values to
+         be passed, or the default None (no keyword arguments are
+         passed).
+
+         Multiple subscribers can be registered and will be called in
+         the order they were registered (first registered, first
+         called). except that hooks registered with different `order`
+         arguments are invoked from smallest `order` value to largest.
+         `order` must be an integer, and defaults to 0.
+
+         For instance, a hook registered with order=1 will be invoked
+         after another hook registered with order=-1 and before
+         another registered with order=2, regardless of which was
+         registered first.  When two hooks are registered with the
+         same order, the first one registered is called first.
+
+         This method can also be called from a subscriber:
+         an executing subscriber can register more subscribers.
+         Applications should take care to avoid creating infinite
+         loops by recursively registering subscribers.
+
+         Subscribers are called only for a top-level commit.  A
+         subtransaction commit or savepoint creation does not call any
+         subscribers.  Calling a subscriber"consumes" its registration:
+         subscriber registrations do not persist across transactions.
+
+         If it's desired to call the same subscriber on every
+         transaction commit, then addSubscriber() must be called with
+         that subscriber during every transaction; in such a case
+         consider registering a synchronizer object via a
+         TransactionManager's registerSynch() method instead.
+         """
+
+        # When the manager is disabled it won't add new subscribers. It
+        # means, it can be deactiveted for a while, thus won't add any
+        # new one, and then be activated again and start adding some
+        # again.
+        if not self._status:
+            self.log.debug("won't register %s with %s and %s with order %s"
+                         %(repr(subscriber), args, kws, str(order)))
+            return
+
+        if not isinstance(order, int):
+            raise ValueError("An integer value is required "
+                             "for the order argument")
+        if kws is None:
+            kws = {}
+
+        if self.isSynchronous():
+            self.log.debug("executs %s with %s and %s" %
+                           (repr(subscriber), args, kws))
+            # False for the fact the the transaction hasn't been commited
+            subscriber(False, *args, **kws)
+            return
+
+        self.log.debug("register %s with %s and %s with order %s"
+                       %(repr(subscriber), args, kws, str(order)))
+        bisect.insort(self._after_commit, (order, self._after_commit_index,
+                                           subscriber, tuple(args), kws))
+        self._after_commit_index += 1
+
+    def __call__(self, status=True):
+        """Execute the registred subscribers
+
+        This method is called by the transaction. Note, the registred
+        subscribers are executed by the
+        AfterCommitSubscribersManager itself and not by the
+        transaction.
+
+        status is the status of the
+        """
+        self.log.debug("__call__")
+
+        while self._after_commit:
+            order, index, subscriber, args, kws = self._after_commit.pop(0)
+            self.log.debug("executs %s with %s and %s" %
+                           (repr(subscriber), args, kws))
+            subscriber(status, *args, **kws)
+        self._after_commit_index = 0
+
+        self.log.debug("__call__ done")
+
+#
+# CPS Helpers
+#
+
 def del_before_commits_subscribers_manager():
     txn = transaction.get()
     setattr(txn, _CPS_BCH_TXN_ATTRIBUTE, None)
@@ -216,4 +359,20 @@ def get_before_commit_subscribers_manager():
     if mgr is None:
         mgr = BeforeCommitSubscribersManager(txn)
         setattr(txn, _CPS_BCH_TXN_ATTRIBUTE, mgr)
+    return mgr
+
+def del_after_commits_subscribers_manager():
+    txn = transaction.get()
+    setattr(txn, _CPS_ACH_TXN_ATTRIBUTE, None)
+
+def get_after_commit_subscribers_manager():
+    """Get the after commit subscribers manager.
+
+    Creates it if needed.
+    """
+    txn = transaction.get()
+    mgr = getattr(txn, _CPS_ACH_TXN_ATTRIBUTE, None)
+    if mgr is None:
+        mgr = AfterCommitSubscribersManager(txn)
+        setattr(txn, _CPS_ACH_TXN_ATTRIBUTE, mgr)
     return mgr
---
+++ tests/test_after_commit_subscribers_manager.py
@@ -0,0 +1,276 @@
+# -*- coding: iso-8859-15 -*-
+# (C) Copyright 2005 Nuxeo SARL <http://nuxeo.com>
+# Author: Julien Anguenot <ja@nuxeo.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+# 02111-1307, USA.
+#
+# $Id$
+"""After commit subscribers manager tests
+"""
+
+import unittest
+
+import transaction
+
+from Products.CPSCore.interfaces import IAfterCommitSubscriber
+from Products.CPSCore.commithooks import _CPS_ACH_TXN_ATTRIBUTE
+from Products.CPSCore.commithooks import AfterCommitSubscribersManager
+from Products.CPSCore.commithooks import get_after_commit_subscribers_manager
+from Products.CPSCore.commithooks import del_after_commits_subscribers_manager
+
+class FakeTransaction:
+    def addAfterCommitHook(self, hook):
+        pass
+
+log = []
+def reset_log():
+    del log[:]
+
+def hook(status=True, arg='no_arg', kw1='no_kw1', kw2='no_kw2'):
+    log.append("status %s arg %r kw1 %r kw2 %r" % (status, arg, kw1, kw2))
+
+class AfterCommitSubscribersManagerTest(unittest.TestCase):
+
+    def test_interfaces(self):
+        from zope.interface.verify import verifyClass
+        verifyClass(IAfterCommitSubscriber, AfterCommitSubscribersManager)
+
+    def test_fixtures(self):
+
+        mgr = AfterCommitSubscribersManager(FakeTransaction())
+
+        self.assertEqual(mgr._after_commit, [])
+        self.assertEqual(mgr._after_commit_index, 0)
+        self.assertEqual(mgr._sync, False)
+        self.assertEqual(mgr.isSynchronous(), False)
+        self.assertEqual(mgr.isSynchronous(), mgr._sync)
+        self.assertEqual(mgr._status, True)
+
+    def test_status_api(self):
+
+        mgr = AfterCommitSubscribersManager(FakeTransaction())
+
+        self.assertEqual(mgr._status, True)
+        mgr.disable()
+        self.assertEqual(mgr._status, False)
+        mgr.enable()
+        self.assertEqual(mgr._status, True)
+
+    def test_async(self):
+
+        mgr = AfterCommitSubscribersManager(FakeTransaction())
+        mgr.addSubscriber(hook, '1')
+
+        self.assertEqual(log, [])
+        mgr()
+        self.assertEqual(log,
+                         ["status True arg '1' kw1 'no_kw1' kw2 'no_kw2'"])
+        reset_log()
+
+    def test_sync(self):
+
+        mgr = AfterCommitSubscribersManager(FakeTransaction())
+        mgr.setSynchronous(True)
+        mgr.addSubscriber(hook, '1')
+
+        self.assertEqual(log,
+                         ["status False arg '1' kw1 'no_kw1' kw2 'no_kw2'"])
+        reset_log()
+
+    def test_status_as_async(self):
+
+        mgr = AfterCommitSubscribersManager(FakeTransaction())
+
+        self.assertEqual(mgr._status, True)
+        self.assertEqual(mgr._sync, False)
+
+        # Disable subscriber
+        mgr.disable()
+
+        # Register hooks
+        mgr.addSubscriber(hook, '1')
+        mgr.addSubscriber(hook, '2')
+        mgr.addSubscriber(hook, '3')
+
+        # Enable subscriber
+        mgr.enable()
+        
+        mgr.addSubscriber(hook, '4')
+        mgr.addSubscriber(hook, '5')
+        mgr.addSubscriber(hook, '6')
+        mgr.addSubscriber(hook, '7')
+
+        # Execute
+        mgr()
+        
+        # Nothing has been executed since it's disabled
+        self.assertEqual(
+            ["status True arg '4' kw1 'no_kw1' kw2 'no_kw2'",
+             "status True arg '5' kw1 'no_kw1' kw2 'no_kw2'",
+             "status True arg '6' kw1 'no_kw1' kw2 'no_kw2'",
+             "status True arg '7' kw1 'no_kw1' kw2 'no_kw2'"],
+            log)
+
+        reset_log()
+
+    def test_status_as_sync(self):
+
+        mgr = AfterCommitSubscribersManager(FakeTransaction())
+
+        self.assertEqual(mgr._status, True)
+        self.assertEqual(mgr._sync, False)
+
+        # Sync = True
+        mgr.setSynchronous(True)
+
+        # Disable subscriber
+        mgr.disable()
+
+        # Register hooks
+        mgr.addSubscriber(hook, '1')
+        mgr.addSubscriber(hook, '2')
+        mgr.addSubscriber(hook, '3')
+
+        # Enable subscriber
+        mgr.enable()
+        
+        mgr.addSubscriber(hook, '4')
+        mgr.addSubscriber(hook, '5')
+        mgr.addSubscriber(hook, '6')
+        mgr.addSubscriber(hook, '7')
+
+        # Nothing has been executed since it's disabled
+        self.assertEqual(
+            ["status False arg '4' kw1 'no_kw1' kw2 'no_kw2'",
+             "status False arg '5' kw1 'no_kw1' kw2 'no_kw2'",
+             "status False arg '6' kw1 'no_kw1' kw2 'no_kw2'",
+             "status False arg '7' kw1 'no_kw1' kw2 'no_kw2'"],
+            log)
+
+        reset_log()
+
+    def test_hooks_registration_without_order(self):
+
+        mgr = AfterCommitSubscribersManager(FakeTransaction())
+
+        # Register hooks
+        mgr.addSubscriber(hook, '1')
+        mgr.addSubscriber(hook, '2')
+        mgr.addSubscriber(hook, '3')
+        mgr.addSubscriber(hook, '4')
+        mgr.addSubscriber(hook, '5')
+        mgr.addSubscriber(hook, '6')
+        mgr.addSubscriber(hook, '7')
+
+        # Execute
+        mgr()
+
+        # The hooks are executed in the order of execution
+        self.assertEqual(
+            ["status True arg '1' kw1 'no_kw1' kw2 'no_kw2'",
+             "status True arg '2' kw1 'no_kw1' kw2 'no_kw2'",
+             "status True arg '3' kw1 'no_kw1' kw2 'no_kw2'",
+             "status True arg '4' kw1 'no_kw1' kw2 'no_kw2'",
+             "status True arg '5' kw1 'no_kw1' kw2 'no_kw2'",
+             "status True arg '6' kw1 'no_kw1' kw2 'no_kw2'",
+             "status True arg '7' kw1 'no_kw1' kw2 'no_kw2'"],
+            log)
+
+        reset_log()
+
+    def test_hooks_registration_with_order(self):
+
+        # Here, we test the ordering policy.
+        # order of registration + order parameter
+
+        mgr = AfterCommitSubscribersManager(FakeTransaction())
+
+        # Register hooks
+        mgr.addSubscriber(hook, '1', order=0)
+        mgr.addSubscriber(hook, '2', order=-999999)
+        mgr.addSubscriber(hook, '3', order=999999)
+        mgr.addSubscriber(hook, '4', order=0)
+        mgr.addSubscriber(hook, '5', order=999999)
+        mgr.addSubscriber(hook, '6', order=-999999)
+        mgr.addSubscriber(hook, '7', order=0)
+
+        # Execute
+        mgr()
+
+        # The hooks are executed in the order of execution
+        self.assertEqual(
+            ["status True arg '2' kw1 'no_kw1' kw2 'no_kw2'",
+             "status True arg '6' kw1 'no_kw1' kw2 'no_kw2'",
+             "status True arg '1' kw1 'no_kw1' kw2 'no_kw2'",
+             "status True arg '4' kw1 'no_kw1' kw2 'no_kw2'",
+             "status True arg '7' kw1 'no_kw1' kw2 'no_kw2'",
+             "status True arg '3' kw1 'no_kw1' kw2 'no_kw2'",
+             "status True arg '5' kw1 'no_kw1' kw2 'no_kw2'"],
+            log)
+
+        reset_log()
+
+class AfterCommitSubscribersManagerIntegrationTest(unittest.TestCase):
+
+    # These really test the beforeCommitHook on a real transaction
+
+    def test_transaction_mecanism(self):
+        transaction.begin()
+        mgr = get_after_commit_subscribers_manager()
+        transaction.commit()
+        self.assertEqual(log, [])
+
+    def test_transaction_with_a_hook(self):
+        transaction.begin()
+        mgr = get_after_commit_subscribers_manager()
+        mgr.addSubscriber(hook, '1')
+        transaction.commit()
+        self.assertEqual(
+            log, ["status True arg '1' kw1 'no_kw1' kw2 'no_kw2'"])
+        reset_log()
+
+    def test_transaction_aborting(self):
+        transaction.begin()
+        mgr = get_after_commit_subscribers_manager()
+        mgr.addSubscriber(hook, '1')
+        transaction.abort()
+        self.assertEqual(log, [])
+
+    def test_get_del_after_commits_subscribers_manager(self):
+        _marker = 'marker'
+
+        # No transaction manager registred over there
+        self.assertEqual(
+            getattr(transaction.get(),
+                    _CPS_ACH_TXN_ATTRIBUTE, _marker), _marker)
+
+        # This will check and add one
+        get_after_commit_subscribers_manager()
+        self.assert_(
+            getattr(transaction.get(), _CPS_ACH_TXN_ATTRIBUTE, None))
+
+        # This will remove it
+        del_after_commits_subscribers_manager()
+        self.assertEqual(
+            getattr(transaction.get(), _CPS_ACH_TXN_ATTRIBUTE, _marker), None)
+
+def test_suite():
+    return unittest.TestSuite((
+        unittest.makeSuite(AfterCommitSubscribersManagerTest),
+        unittest.makeSuite(AfterCommitSubscribersManagerIntegrationTest),
+        ))
+
+if __name__ == '__main__':
+    unittest.TextTestRunner().run(test_suite())
