a797a60269dec69472240eb8b7e86514c25dfc99
fguillaume <devnull@localhost>
2006-01-22 01:25:01 +0000

    Protect the repository.

--- ObjectRepositoryTool.py
+++ ObjectRepositoryTool.py
@@ -81,6 +81,7 @@ class ObjectRepositoryTool(UniqueObject,
     portal_type = meta_type
 
     security = ClassSecurityInfo()
+    security.declareObjectPrivate()
 
     def __init__(self):
         BTreeFolder2.__init__(self, self.id)
