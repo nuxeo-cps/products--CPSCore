15ce2b53fc5881798991870ea78733a08467e159
madarche <devnull@localhost>
2006-09-18 08:54:22 +0000

    Forgot to commit this file while removing deprecated _isinstance.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -6,14 +6,6 @@ import unittest
 
 class Test(unittest.TestCase):
 
-    def test_isinstance(self):
-        class A: pass
-        class B: pass
-        a = A()
-        b = B()
-        self.assert_(utils._isinstance(a, A))
-        self.failIf(utils._isinstance(b, A))
-
     def test_mergedLocalRolesManipulation(self):
         # The _mergedLocalRoles function used to return references to
         # actual local role settings and it was possible to manipulate them
