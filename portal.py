13105b0b497aebf6c0e9e567aa15483f3093290d
madarche <devnull@localhost>
2009-03-05 14:22:30 +0000

    Setting for 3.5.0.

--- portal.py
+++ portal.py
@@ -36,7 +36,7 @@ class CPSSite(CMFSite):
     meta_type = 'CPS Site'
     portal_type = 'Portal'
 
-    cps_version = ('CPS', 3, 4, 8)
+    cps_version = ('CPS', 3, 5, 0)
 
     _properties = (
         {'id': 'title', 'type': 'string',
