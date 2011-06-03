573e92d67cbc71dab1d19db17bf79ebad71639d4
Georges Racinet on purity.racinet.fr <georges@racinet.fr>
2011-06-03 13:05:55 +0200

    test for #2402

--- tests/test_proxy.py
+++ tests/test_proxy.py
@@ -611,6 +611,27 @@ class ProxyTraversalTest(ZopeTestCase):
         img_content = dl.index_html(req, req.RESPONSE)
         self.assertEquals(dl.content_type(), 'image/png')
 
+    def test_img_downloader_zope_file(self):
+        # the img downloader must also work with OFS.Image.File objects
+        proxy = self.proxy
+        doc = proxy.getContent()
+        f = open(os.path.join(TEST_DATA_PATH, 'logo_cps.png'))
+        doc._setObject('fobj', File('fobj', 'myimg.png', f))
+        # we assume that content-type is correct on the file object
+        doc.fobj.content_type = 'image/png'
+
+        dl = proxy[KEYWORD_SIZED_IMAGE]
+        dl.__bobo_traverse__(None, 'fobj')
+        dl.__bobo_traverse__(None, '320x200')
+        dl.__bobo_traverse__(None, 'hisimg.png')
+
+        self.assertFalse(dl.isFullSize())
+        self.assertRaises(BadRequest, dl.assertFullSize, meth_name='TEST')
+
+        req = self.folder.REQUEST
+        img_content = dl.index_html(req, req.RESPONSE)
+        self.assertEquals(dl.content_type(), 'image/png')
+
     def test_img_downloader_largest(self):
         proxy = self.proxy
         doc = proxy.getContent()
