3bf3b70048aeddd7e3bd7cd6a53dee3eecfc4b1e
Georges Racinet on purity.racinet.fr <georges@racinet.fr>
2011-09-23 00:40:13 +0200

    fixed new dev version for 3.5 branch

--- portal.py
+++ portal.py
@@ -36,8 +36,8 @@ class CPSSite(CMFSite):
     meta_type = 'CPS Site'
     portal_type = 'Portal'
 
-    cps_version = ('CPS', 3, 5, 3)
-    cps_version_suffix = ''
+    cps_version = ('CPS', 3, 5, 4)
+    cps_version_suffix = 'dev'
 
     _properties = (
         {'id': 'title', 'type': 'ustring',
