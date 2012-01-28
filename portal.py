e01f941dbb1c3d432285273efb1f8bf165e87080
Georges Racinet on ishtar.racinet.fr <georges@racinet.fr>
2012-01-28 19:11:12 +0100

    Set portal version to CPS 3.5.5

--- portal.py
+++ portal.py
@@ -36,7 +36,7 @@ class CPSSite(CMFSite):
     meta_type = 'CPS Site'
     portal_type = 'Portal'
 
-    cps_version = ('CPS', 3, 5, 4)
+    cps_version = ('CPS', 3, 5, 5)
     cps_version_suffix = ''
 
     _properties = (
